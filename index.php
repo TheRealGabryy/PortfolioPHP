1455cd79a1042612209d90a71919f138fe32e705
Gabriele B <the.real.gabryy@gmail.com>
2025-12-03 05:50:45 +0100

    Make the HTML structure and started CSS.
    INCOMPLETE CSS, completet HTML, only thing missing is the Footer.
    
    First animation with @keyframes

--- index.php
+++ index.php
@@ -6,29 +6,33 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Final TRG</title>
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
 <header class="header">
     <div class="container">
         <div class="logo">
-            <img src="logo.png" alt="logo">
+            <img src="Assets/IMG/logo.png" alt="logo" class="img-logo">
         </div>
-        <div class="title">
-            <p class="title-text">
-                Enterprise web dev
-            </p>
-        </div>
-        <div class="subtitle">
-            <p class="subtitle-text">
-                TheRealG
-            </p>
-        </div>
-        <div class="action-button">
-            <button class="contact-button">
-                Contact me
-            </button>
+        <div class="header-container">
+            <div class="title">
+                <p class="title-text">
+                    Enterprise web dev
+                </p>
+            </div>
+            <div class="subtitle">
+                <p class="subtitle-text">
+                    TheRealG
+                </p>
+            </div>
         </div>
     </div>
+    <div class="action-button">
+        <button class="contact-button">
+            Contact me
+        </button>
+    </div>
+
 </header>
 <main class="main">
     <section class="who-am-i">
@@ -43,9 +47,168 @@
             </div>
             <div class="section-text">
                 <p class="section-paragraph-text">
-                    I'm an 18 year old developer that started at a very young age (13) programming in C#, later transitioned to Java, then python, then the web dev stack.
-
+                    I'm an 18-year-old developer that started at a very young age (13) programming in C#, later
+                    transitioned to Java, then python, then the web dev stack.
                 </p>
+            </div>
+            <div class="cta-button">
+                <button class="view-github">
+                    View GitHub
+                </button>
+            </div>
+        </div>
+    </section>
+
+    <section class="know-me-better">
+        <div class="section-title">
+            <p class="section-title-text">
+                Know me better
+            </p>
+            <p class="section-subtitle-text">
+                With a simple showreel
+            </p>
+        </div>
+        <div class="showreel-container">
+
+        </div>
+        <div class="cta-button">
+            <button class="contact-button">
+                Contact me
+            </button>
+        </div>
+    </section>
+
+    <section class="main-project">
+        <div class="section-title">
+            <p class="section-title-text">
+                Main Project
+            </p>
+        </div>
+        <div class="project-container">
+            <div class="gallery-carousel">
+                <!--TODO: carousel in JS-->
+            </div>
+
+            <div class="project-description">
+                <p class="project-description-text">
+                    FlashFX is an AI powered motion graphics software designed to edit FASTER, lighter, and easier for
+                    everyone
+                </p>
+            </div>
+
+            <div class="development-status">
+                <!--TODO: development status progress bar-->
+            </div>
+
+            <div class="button-contauner">
+                <div class="cta-button">
+                    <button class="more-info-button">
+                        More Info
+                    </button>
+                </div>
+
+                <div class="cta-button">
+                    <button class="contact-button">
+                        Contact me
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="skills">
+        <div class="section-title">
+            <p class="section-title-text">
+                My Skills
+            </p>
+        </div>
+
+        <div class="skills-container">
+            <p class="skill-category">
+                Web Dev stack
+            </p>
+            <div class="skill-box">
+                <p class="skill-title">
+                    React
+                </p>
+                <p class="skill-level">
+                    Advanced
+                </p>
+                <button class="show-project-button">
+                    Show projects
+                </button>
+            </div>
+            <div class="skill-box">
+                <p class="skill-title">
+                    TypeScript
+                </p>
+                <p class="skill-level">
+                    Intermediate
+                </p>
+                <button class="show-project-button">
+                    Show projects
+                </button>
+            </div>
+            <div class="skill-box">
+                <p class="skill-title">
+                    HTML/CSS/JS
+                </p>
+                <p class="skill-level">
+                    mastered
+                </p>
+                <button class="show-project-button">
+                    Show projects
+                </button>
+            </div>
+            <div class="skill-box">
+                <p class="skill-title">
+                    PHP
+                </p>
+                <p class="skill-level">
+                    intermediate
+                </p>
+                <button class="show-project-button">
+                    Show projects
+                </button>
+            </div>
+        </div>
+
+        <div class="skills-container">
+            <p class="skill-category">
+                Programming languages
+            </p>
+            <div class="skill-box">
+                <p class="skill-title">
+                    Java
+                </p>
+                <p class="skill-level">
+                    Advanced
+                </p>
+                <button class="show-project-button">
+                    Show projects
+                </button>
+            </div>
+            <div class="skill-box">
+                <p class="skill-title">
+                    Python
+                </p>
+                <p class="skill-level">
+                    Intermediate
+                </p>
+                <button class="show-project-button">
+                    Show projects
+                </button>
+            </div>
+            <div class="skill-box">
+                <p class="skill-title">
+                    C/C++
+                </p>
+                <p class="skill-level">
+                    intermediate
+                </p>
+                <button class="show-project-button">
+                    Show projects
+                </button>
             </div>
         </div>
     </section>
