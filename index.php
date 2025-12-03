61bc95c4ce1d090c1e1836a6732629c9bcc12017
Gabriele B <the.real.gabryy@gmail.com>
2025-12-03 04:56:55 +0100

    Init Commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Final TRG</title>
+</head>
+<body>
+<header class="header">
+    <div class="container">
+        <div class="logo">
+            <img src="logo.png" alt="logo">
+        </div>
+        <div class="title">
+            <p class="title-text">
+                Enterprise web dev
+            </p>
+        </div>
+        <div class="subtitle">
+            <p class="subtitle-text">
+                TheRealG
+            </p>
+        </div>
+        <div class="action-button">
+            <button class="contact-button">
+                Contact me
+            </button>
+        </div>
+    </div>
+</header>
+<main class="main">
+    <section class="who-am-i">
+        <div class="container">
+            <div class="section-logo">
+                <img src="logo.png" alt="logo">
+            </div>
+            <div class="section-title">
+                <p class="section-title-text">
+
+                </p>
+            </div>
+            <div class="section-text">
+                <p class="section-paragraph-text">
+                    I'm an 18 year old developer that started at a very young age (13) programming in C#, later transitioned to Java, then python, then the web dev stack.
+                </p>
+            </div>
+        </div>
+    </section>
+</main>
+<footer class="footer">
+
+</footer>
+</body>
+</html>
+
+
+<?php
